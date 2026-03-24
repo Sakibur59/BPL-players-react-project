2db2e20e317faee2400c3e202e032340d6d92521
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-24 20:35:39 +0600

    available section are added

--- src/Components/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/AvailablePlayers/AvailablePlayers.jsx
@@ -3,7 +3,7 @@ import { FaFlag, FaIcons, FaUser } from "react-icons/fa";
 
 const AvailablePlayers = ({ playerData }) => {
   return (
-    <div className="max-w-300 mx-auto mt-25">
+    <div className="max-w-300 mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
         {playerData.map((player) => {
           return (
--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -5,7 +5,17 @@ const Players = ({playerPromise}) => {
     const playerData = use(playerPromise);
     
     return (
-        <div className='max-w-[300] mx-auto'>
+        <div className='max-w-300 mx-auto my-4'>
+
+            <div className='flex justify-between gap-2 items-center mb-10'>
+                
+                    <h2 className='font-bold'>Available Players</h2>
+                <div>
+                    <button className='btn bg-[#E7FE29] rounded-r-none rounded-l-xl'>Available</button>
+                    <button className='btn rounded-l-none rounded-r-xl'>selected(0)</button>
+                </div>
+                
+            </div>
             <AvailablePlayers playerData={playerData}></AvailablePlayers>
         </div>
     );
