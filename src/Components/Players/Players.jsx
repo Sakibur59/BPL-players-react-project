ca91a2ebf2bb127d23199a830807bd2657b0e6e8
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-25 10:47:58 +0600

    toggole to selected player added

--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -1,5 +1,6 @@
 import React, { use, useState } from "react";
 import AvailablePlayers from "../AvailablePlayers/AvailablePlayers";
+import SelectedPlayer from "../SelectedPlayer/SelectedPlayer";
 
 const Players = ({ playerPromise }) => {
   const playerData = use(playerPromise);
@@ -8,7 +9,11 @@ const Players = ({ playerPromise }) => {
   return (
     <div className="max-w-300 mx-auto my-4">
       <div className="flex justify-between gap-2 items-center mb-10">
-        <h2 className="font-bold">Available Players</h2>
+        {selectedType === "available" ? (
+          <h2 className="font-bold">Available Players</h2>
+        ) : (
+          <h2 className="font-bold">Selected Player (0/12)</h2>
+        )}
         <div>
           <button
             onClick={() => setSelectedType("available")}
@@ -18,13 +23,15 @@ const Players = ({ playerPromise }) => {
           </button>
           <button
             onClick={() => setSelectedType("selected")}
-            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : ""}rounded-l-none rounded-r-xl`}
+            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}
           >
             selected(0)
           </button>
         </div>
       </div>
-      <AvailablePlayers playerData={playerData}></AvailablePlayers>
+      {
+        selectedType === "available" ? <AvailablePlayers playerData={playerData}></AvailablePlayers> : <SelectedPlayer></SelectedPlayer>
+      }
     </div>
   );
 };
---
+++ src/Components/SelectedPlayer/SelectedPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayer = () => {
+    return (
+        <div>
+            hhh
+        </div>
+    );
+};
+
+export default SelectedPlayer;
